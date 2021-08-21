1b58ebdd21e71f18fc6529328ae85ecf824d8e8c
Harry Owens <harry@owens.codes>
2021-08-21 15:46:40 +0000

    Add deck values and suits

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 class Table:
     """
-    Primary class for all Blackjack logic.
     Holds the player's chip stack and card deck(s).
     Maintains game state and has methods for displaying game status to the user.
     """
@@ -14,5 +13,14 @@ class Deck:
     Standard 52 card deck (indexed 0 to 51)
     """
 
+    card_value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    card_suit = ['C', 'D', 'H', 'S']
+
     def __init__(self):
         self.cards = range(51)
+
+    def get_value(self, x):
+        return x % 4
+
+    def get_suit(self, x):
+        return x % 13
