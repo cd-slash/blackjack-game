f2309868f2063abb827f65514aba2161299ab64b
Harry Owens <harry@owens.codes>
2021-08-22 08:26:33 +0000

    Update deck logic and add shoe class to hold decks

--- run.py
+++ run.py
@@ -10,17 +10,39 @@ class Table:
 
 class Deck:
     """
-    Standard 52 card deck (indexed 0 to 51)
+    Standard 52 card deck
     """
 
-    card_value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    card_suit = ['C', 'D', 'H', 'S']
+    card_values = [
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'
+        ]
+
+    card_suits = [
+        'C', 'D', 'H', 'S'
+        ]
 
     def __init__(self):
-        self.cards = range(51)
+        self.cards = range(52)
+        random.shuffle(self.cards)
 
+    # card values are in 0-12 indexed array
+    # 4 suits of 13 cards, so value is remainder after dividing by 13
     def get_value(self, x):
-        return x % 4
+        return self.card_values[x % 13]
 
+    # 52 cards in 4 suits, so round down after dividing by 13 for suit
     def get_suit(self, x):
-        return x % 13
+        return self.card_suits[math.floor(x / 13)]
+
+
+class Shoe:
+    """
+    Contains one or more decks of cards and a cut point that defines when a
+    reshuffle (i.e. a new shoe) is needed
+    """
+
+    def __init__(self, num_decks):
+        self.cards = []
+        for _ in range(num_decks):
+            new_deck = Deck()
+            self.cards += new_deck.cards
